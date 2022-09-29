c5b1f41b34de6834e466925113f451d569ce7358
fekete-gabor <91623519+fekete-gabor@users.noreply.github.com>
2022-09-29 13:51:37 +0200

    Revert "0"
    This reverts commit 7ce621eb250c7a9cdabc23e03b13b374744f1fe6.

--- src/components/Modal.js
+++ src/components/Modal.js
@@ -407,7 +407,7 @@ const Wrapper = styled.div`
     height: fit-content;
     background-color: red;
     margin: 0 auto;
-    position: fixed;
+    position: absolute;
     bottom: 0;
     display: flex;
     justify-content: center;
