d7b0d2f0f71cb1c1c80ff9c535e797082e78a9fe
fekete-gabor <91623519+fekete-gabor@users.noreply.github.com>
2022-09-29 10:13:44 +0200

    test

--- src/components/Gallery.js
+++ src/components/Gallery.js
@@ -29,7 +29,11 @@ const Gallery = () => {
     const container = document.querySelector(".gallery-img-container");
 
     container.addEventListener("mouseleave", () => {
-      gsap.to(".gallery-img", { filter: "none" });
+      gsap.fromTo(
+        ".gallery-img",
+        { filter: "grayscale(100%)" },
+        { filter: "grayscale(0%)" }
+      );
     });
   }, []);
 
