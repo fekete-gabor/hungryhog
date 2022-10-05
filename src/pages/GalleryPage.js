f6b79f851ebaf7e5e9e239c3aa2798e015574687
fekete-gabor <91623519+fekete-gabor@users.noreply.github.com>
2022-10-05 10:41:14 +0200

    added scroll top

--- src/pages/GalleryPage.js
+++ src/pages/GalleryPage.js
@@ -1,9 +1,14 @@
+import { useEffect } from "react";
 import { Gallery, Modal } from "../components/";
 import { useSelector } from "react-redux/es/exports";
 import styled from "styled-components";
 
 const GalleryPage = () => {
   const { isModal } = useSelector((store) => store.modal);
+
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, []);
 
   return <Wrapper>{isModal ? <Modal /> : <Gallery />}</Wrapper>;
 };
